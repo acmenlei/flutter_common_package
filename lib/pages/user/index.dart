9b42ca36e5db83f1b99fe2ecdf5f3c9b7fa554f0
lx <coderleilei@163.com>
2024-11-07 23:22:54 +0800

    feat: 用户主页ui

--- lib/pages/user/index.dart
+++ lib/pages/user/index.dart
@@ -1,5 +1,7 @@
 import 'package:codefather_app/components/common_tabbar_layout/index.dart';
 import 'package:codefather_app/components/user_avatar/index.dart';
+import 'package:codefather_app/components/user_title/index.dart';
+import 'package:codefather_app/constants/colors.dart';
 import 'package:codefather_app/pages/user/constants.dart';
 import 'package:codefather_app/pages/user/views/comment.dart';
 import 'package:codefather_app/pages/user/views/fens.dart';
@@ -25,36 +27,11 @@ class UserPage extends StatelessWidget {
         List<String> renderTabs =
             userController.isMy.value ? userPageTabs : otherUserPageTabs;
         List<Widget> renderViews = getRenderTabViews(userController);
-
         final userData = userController.data.value;
 
         return CommonTabBarLayout(
           sliverBuilder: (context, innerBoxIsScrolled) => [
-            SliverAppBar(
-              floating: true,
-              snap: true,
-              expandedHeight: 140,
-              flexibleSpace: FlexibleSpaceBar(
-                centerTitle: true,
-                background: Container(
-                  color: getPrimaryColor(),
-                  height: double.infinity,
-                  width: double.infinity,
-                ),
-                title: ClipOval(
-                  child: UserAvatar(
-                    user: userData,
-                    size: 30,
-                  ),
-                ),
-                collapseMode: CollapseMode.pin,
-                stretchModes: const [
-                  StretchMode.blurBackground,
-                  StretchMode.fadeTitle,
-                  StretchMode.zoomBackground
-                ],
-              ),
-            ),
+            _buildAppBar(userData), /*  _buildUserInfo(userData) */
           ],
           tabs: renderTabs.map((tab) => Tab(text: tab)).toList(),
           tabViewList: renderViews,
@@ -81,4 +58,58 @@ class UserPage extends StatelessWidget {
             UserFensView(user: userData)
           ];
   }
+
+  // 构建用户头像主要信息
+  _buildAppBar(userData) {
+    return SliverAppBar(
+      surfaceTintColor: Colors.transparent,
+      backgroundColor: Colors.white,
+      floating: true,
+      snap: true,
+      toolbarHeight: 168, // 设置 AppBar 的高度
+      expandedHeight: 168, // 和 toolbarHeight 一样高就不会导致内容溢出
+      flexibleSpace: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          colors: [getPrimaryColor(), Colors.white10],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        )),
+        width: Get.width,
+        height: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16, right: 16, top: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              UserAvatar(
+                user: userData,
+                size: 30,
+              ),
+              const SizedBox(height: 8),
+              UserTitle(
+                user: userData,
+                color: secondaryColor,
+              ),
+              const SizedBox(height: 8),
+              Text(
+                userData.userProfile ?? '',
+                style: const TextStyle(color: tertiaryColor),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // 构建用户次要信息
+  // _buildUserInfo(userData) {
+  //   return SliverToBoxAdapter(
+  //       child: Container(
+  //     padding: const EdgeInsets.symmetric(horizontal: 16),
+  //     child: const Text(''),
+  //   ));
+  // }
 }
